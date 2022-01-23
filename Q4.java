62a6d6059ae17811d7fd9dd42f7da39ca4b7cfd1
Aayush3014 <0201csai036@niet.co.in>
2022-01-23 20:04:13 +0530

    commit

---
+++ Q4.java
@@ -0,0 +1,10 @@
+public class Q4 {
+    public static void main(String[] args) {
+        double k = 45.5f;
+        double m = k * 0.6214;
+
+        System.out.println(k + " Kilometers is = " + m + " miles.");
+
+    }
+
+}

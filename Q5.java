f933cb4c81764aa6cc2b920531a321b6abc1f3dd
Aayush3014 <0201csai036@niet.co.in>
2022-01-23 21:19:49 +0530

    commit

---
+++ Q5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int s = sc.nextInt();
+        boolean b = sc.hasNextInt();
+       // System.out.println(s);
+        System.out.println(b);
+        sc.close();
+    }
+
+}

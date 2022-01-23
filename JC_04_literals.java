5791eb697b812c279342c3c06b6368f6999631f2
Aayush3014 <0201csai036@niet.co.in>
2022-01-23 18:52:09 +0530

    commit

--- JC_04_literals.java
+++ JC_04_literals.java
@@ -1,11 +1,16 @@
-public class JC_04_literals{
+public class JC_04_literals {
     public static void main(String[] args) {
-        int a = 45;
+        int a = 455454545;
         byte b = 48;
         char c = 'a';
-        double d = 89.65;
-        
+        double d = 89.65d;
+        float f = 4.25f;
+        long l = 5444444444444444444l;
+        boolean bb = true;
+        String s = "isfjsfdj";
 
+        System.out.println(a);
+        System.out.print(s);
 
     }
 }

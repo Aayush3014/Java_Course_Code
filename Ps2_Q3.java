7ed8804ff154cd887c46c5820ce98c5eaa9b736f
Aayush3014 <0201csai036@niet.co.in>
2022-01-24 20:54:19 +0530

    commit

--- Ps2_Q3.java
+++ Ps2_Q3.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
-public class Ps2_Q3 {
+public class Ps2_commit
+Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.println(a>8);
+        sc.close();
     }
 }
